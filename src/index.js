d801f6b85f1f8bb52b0930f7586341c8004f2dee
RemAntof <anton.ramarchuk@gmail.com>
2023-10-29 18:46:28 +0100

    done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+   const length = 10
+   const length_expr = expr.split('').length
+   final = ""
+   for(var i = 0 ;i<length_expr/length;i++){
+    var word = expr.slice(i*length,(i+1)*length)
+    if (word==='**********'){
+        final= final + " "
+    }else{
+        var key = ""
+        var test = ""
+        for(var t =  0; t<length/2;t++){
+            var two = word.slice(t*2,(t+1)*2)
+            // process.stdout.write("  " +two)
+
+            if(two!=="00"){
+                if(two === "10"){
+                    key = key + '.'
+                }else{
+                    key = key + '-'
+                }
+            }
+
+        }
+        process.stdout.write("  " +key)
+        final = final + MORSE_TABLE[key]
+    }
+
+   }
+   return final
 }
 
 module.exports = {
